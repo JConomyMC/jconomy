e42a791c0b352644053470d720d929423b3d6eb8
Matthew Renze <merenze@gmail.com>
2025-12-13 12:41:51 -0600

    feat: create database migrator

---
+++ src/main/java/com/jellyrekt/jconomy/storage/SqliteDatabaseMigrator.java
@@ -0,0 +1,11 @@
+package com.jellyrekt.jconomy.storage;
+
+public class SqliteDatabaseMigrator implements DatabaseMigrator {
+
+    @Override
+    public void migrate() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'migrate'");
+    }
+    
+}
