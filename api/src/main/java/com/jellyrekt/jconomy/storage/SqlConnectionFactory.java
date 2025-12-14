b704cf88fb2ddbb4b5ab04f13668ffc1a07da096
Matthew Renze <merenze@gmail.com>
2025-12-14 00:08:16 -0600

    feat: implement connection factory

---
+++ api/src/main/java/com/jellyrekt/jconomy/storage/SqlConnectionFactory.java
@@ -0,0 +1,8 @@
+package com.jellyrekt.jconomy.storage;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface  SqlConnectionFactory {
+    Connection createConnection() throws SQLException;
+}
