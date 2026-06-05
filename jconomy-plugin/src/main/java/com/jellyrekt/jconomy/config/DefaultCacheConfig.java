a45c76667efbaa909ec934ba0960b714efd23b6c
Matthew Renze <merenze@gmail.com>
2026-06-05 14:14:48 -0500

    fixL DefaultCacheConfig: receuves  JConomyConfig, reads LRU limit from correct key

--- jconomy-plugin/src/main/java/com/jellyrekt/jconomy/config/DefaultCacheConfig.java
+++ jconomy-plugin/src/main/java/com/jellyrekt/jconomy/config/DefaultCacheConfig.java
@@ -1,29 +1,14 @@
 package com.jellyrekt.jconomy.config;
 
-import com.jellyrekt.storage.fileconfiguration.FileConfigurationProvider;
-import com.jellyrekt.storage.fileconfiguration.FileConfigurationStorage;
+public class DefaultCacheConfig implements CacheConfig {
+    private final JConomyConfig config;
 
-public class DefaultCacheConfig extends FileConfigurationStorage implements CacheConfig {
-    private final String baseKey = "cache";
-
-    public DefaultCacheConfig(FileConfigurationProvider configurationProvider) {
-        super(configurationProvider);
-        //TODO Auto-generated constructor stub
+    public DefaultCacheConfig(JConomyConfig config) {
+        this.config = config;
     }
 
     @Override
     public int getLruLimit() {
-        return getInt("lru-limit", 10000);
+        return config.getSection("cache").getInt("lru-limit", 10000);
     }
-    
-    private int getInt(String key, int defaultValue) {
-        return getFileConfiguration().getInt(getFullKey(key), defaultValue);
-    }
-
-    private String getFullKey(String partialKey) {
-        return baseKey == null || baseKey == ""
-            ? partialKey
-            : baseKey;
-    }
-        
 }
