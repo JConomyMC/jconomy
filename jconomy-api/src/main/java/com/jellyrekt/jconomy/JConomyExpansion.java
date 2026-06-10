74a9ab0675fae2a6130f027ae1caf5d601a75f92
Matthew Renze <merenze@gmail.com>
2026-06-09 20:54:00 -0500

    feat: add onServicesReady lifecycle method to JConomyExpansion and AbstractJConomyExpansion

--- jconomy-api/src/main/java/com/jellyrekt/jconomy/JConomyExpansion.java
+++ jconomy-api/src/main/java/com/jellyrekt/jconomy/JConomyExpansion.java
@@ -1,8 +1,10 @@
 package com.jellyrekt.jconomy;
 
 import com.jellyrekt.jconomy.dependencyinjection.JConomyServiceBuilder;
+import com.jellyrekt.jconomy.dependencyinjection.JConomyServiceProvider;
 
 public interface JConomyExpansion {
     String getName();
     void configureServices(JConomyServiceBuilder builder);
+    void onServicesReady(JConomyServiceProvider provider);
 }
