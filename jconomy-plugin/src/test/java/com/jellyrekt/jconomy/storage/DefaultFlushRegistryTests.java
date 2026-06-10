6eda995aa8e2d98a9b3f67a238f9a1c9372d947b
Matthew Renze <merenze@gmail.com>
2026-06-09 20:50:05 -0500

    feat: add FlushRegistry interface and DefaultFlushRegistry

---
+++ jconomy-plugin/src/test/java/com/jellyrekt/jconomy/storage/DefaultFlushRegistryTests.java
@@ -0,0 +1,43 @@
+package com.jellyrekt.jconomy.storage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DefaultFlushRegistryTests {
+
+    private DefaultFlushRegistry registry;
+
+    @BeforeEach
+    void setUp() {
+        registry = new DefaultFlushRegistry();
+    }
+
+    @Test
+    void flushAll_calls_flush_on_registered_flushable() {
+        var flushed = new boolean[] { false };
+        registry.register(() -> flushed[0] = true);
+
+        registry.flushAll();
+
+        assertTrue(flushed[0]);
+    }
+
+    @Test
+    void flushAll_calls_flush_on_all_registered_flushables() {
+        var flushCount = new int[] { 0 };
+        registry.register(() -> flushCount[0]++);
+        registry.register(() -> flushCount[0]++);
+        registry.register(() -> flushCount[0]++);
+
+        registry.flushAll();
+
+        assertEquals(3, flushCount[0]);
+    }
+
+    @Test
+    void flushAll_does_nothing_when_no_flushables_registered() {
+        assertDoesNotThrow(() -> registry.flushAll());
+    }
+}
