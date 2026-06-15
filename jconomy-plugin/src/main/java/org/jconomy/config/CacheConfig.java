5af9a7b4138cdeb4acea7ccf60632a0cd25a1f74
Matthew Renze <merenze@gmail.com>
2026-06-15 18:59:11 -0500

    feat: add PeriodicFlushConfig with enabled and interval-ticks settings

--- jconomy-plugin/src/main/java/org/jconomy/config/CacheConfig.java
+++ jconomy-plugin/src/main/java/org/jconomy/config/CacheConfig.java
@@ -2,4 +2,11 @@ package org.jconomy.config;
 
 public interface CacheConfig {
     int getLruLimit();
+
+    PeriodicFlushConfig getPeriodicFlushConfig();
+
+    public interface PeriodicFlushConfig {
+        boolean isEnabled();
+        int getIntervalTicks();
+    }
 }
