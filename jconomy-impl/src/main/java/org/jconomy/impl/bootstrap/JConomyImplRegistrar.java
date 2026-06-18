43777612f5701e6ee8f19377b5323c854186be1e
Matthew Renze <merenze@gmail.com>
2026-06-18 01:54:31 -0500

    refactor: move flush registry to impl module and wire plugin registrar

--- jconomy-impl/src/main/java/org/jconomy/impl/bootstrap/JConomyImplRegistrar.java
+++ jconomy-impl/src/main/java/org/jconomy/impl/bootstrap/JConomyImplRegistrar.java
@@ -1,6 +1,8 @@
 package org.jconomy.impl.bootstrap;
 
 import org.jconomy.dependencyinjection.JConomyServiceBuilder;
+import org.jconomy.storage.DefaultFlushRegistry;
+import org.jconomy.storage.FlushRegistry;
 
 public final class JConomyImplRegistrar {
 
@@ -11,5 +13,7 @@ public final class JConomyImplRegistrar {
         if (builder == null) {
             throw new IllegalArgumentException("builder cannot be null");
         }
+
+        builder.addSingleton(FlushRegistry.class, DefaultFlushRegistry.class);
     }
 }
--- jconomy-impl/src/test/java/org/jconomy/impl/bootstrap/JConomyImplRegistrarTests.java
+++ jconomy-impl/src/test/java/org/jconomy/impl/bootstrap/JConomyImplRegistrarTests.java
@@ -1,14 +1,20 @@
 package org.jconomy.impl.bootstrap;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.function.Function;
 
 import org.junit.jupiter.api.Test;
 
 import org.jconomy.dependencyinjection.JConomyServiceBuilder;
 import org.jconomy.dependencyinjection.JConomyServiceProvider;
+import org.jconomy.storage.FlushRegistry;
+import org.jconomy.storage.DefaultFlushRegistry;
 
 class JConomyImplRegistrarTests {
 
@@ -19,7 +25,18 @@ class JConomyImplRegistrarTests {
         assertDoesNotThrow(() -> JConomyImplRegistrar.registerServices(builder));
     }
 
+    @Test
+    void registerServices_registers_default_flush_registry() {
+        var builder = new NoopBuilder();
+
+        JConomyImplRegistrar.registerServices(builder);
+
+        assertTrue(builder.singletonImplementations.containsKey(FlushRegistry.class));
+        assertEquals(DefaultFlushRegistry.class, builder.singletonImplementations.get(FlushRegistry.class));
+    }
+
     private static final class NoopBuilder implements JConomyServiceBuilder {
+        private final Map<Class<?>, Class<?>> singletonImplementations = new HashMap<>();
 
         @Override
         public <T> JConomyServiceBuilder addSingleton(Class<T> type) {
@@ -28,6 +45,7 @@ class JConomyImplRegistrarTests {
 
         @Override
         public <S, T extends S> JConomyServiceBuilder addSingleton(Class<S> type, Class<T> implementationType) {
+            singletonImplementations.put(type, implementationType);
             return this;
         }
 
