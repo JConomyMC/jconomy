529b2592fc1595983fbaa52ecee364ff7e5549b4
Matthew Renze <merenze@gmail.com>
2026-06-24 09:24:12 -0500

    refactor: add ConfigMigrator with Builder skeleton

---
+++ bukkit-configuration-migration/src/main/java/com/jellyrekt/configuration/migration/ConfigMigrator.java
@@ -0,0 +1,50 @@
+package com.jellyrekt.configuration.migration;
+
+import org.bukkit.configuration.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ConfigMigrator {
+
+    private final String versionPath;
+    private final List<RegisteredConfigMigration> migrations;
+
+    private ConfigMigrator(String versionPath, List<RegisteredConfigMigration> migrations) {
+        this.versionPath = versionPath;
+        this.migrations = migrations;
+    }
+
+    public void migrate(Configuration config) {
+    }
+
+    public static Builder builder(String versionPath) {
+        return new Builder(versionPath);
+    }
+
+    private record RegisteredConfigMigration(int targetVersion, ConfigMigration migration) {}
+
+    public static class Builder {
+
+        private final String versionPath;
+        private final List<ConfigMigration> migrations = new ArrayList<>();
+
+        private Builder(String versionPath) {
+            this.versionPath = versionPath;
+        }
+
+        public Builder addNext(ConfigMigration migration) {
+            migrations.add(migration);
+            return this;
+        }
+
+        public ConfigMigrator build() {
+            var registered = new ArrayList<RegisteredConfigMigration>();
+            for (int i = 0; i < migrations.size(); i++) {
+                registered.add(new RegisteredConfigMigration(i + 1, migrations.get(i)));
+            }
+            return new ConfigMigrator(versionPath, Collections.unmodifiableList(registered));
+        }
+    }
+}
